a6d6ba4670dad461c7c3d48320d0c73fa7cd4470
lukasborawski <lukas.borawski@gmail.com>
2020-10-18 15:14:12 +0200

    chore: init

---
+++ components/Notes/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul data-testid="test-notes">
+    <app-note v-for="(item, index) in notesProp" :key="index" :item-prop="item" />
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+import { Note } from '~/types'
+import AppNote from './Note.vue'
+
+const notesProp: PropOptions<Note[]> = {
+  type: Array
+}
+
+export default Vue.extend({
+  name: 'AppNotesList',
+  components: {
+    AppNote
+  },
+  props: {
+    notesProp
+  },
+  data: () => ({}),
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/Notes/Note.vue
@@ -0,0 +1,69 @@
+<template>
+  <transition name="fade">
+    <li v-show="visible" class="message mb-5" data-testid="test-note">
+      <div class="message-header">
+        <p>{{ itemProp.title }}</p>
+        <button class="delete" aria-label="delete" @click="remove" data-testid="test-note-remove" />
+      </div>
+      <div class="message-body">
+        <p>{{ itemProp.note }}</p>
+        <span class="is-size-7 is-date">{{ dateFormatted }}</span>
+      </div>
+    </li>
+  </transition>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+import { Note } from '~/types'
+import { mapActions } from 'vuex'
+// @ts-ignore
+import dayjs from 'dayjs'
+
+const itemProp: PropOptions<Note> = {
+  type: Object,
+}
+
+export default Vue.extend({
+  name: 'AppNote',
+  props: {
+    itemProp,
+  },
+  data: (): { visible: boolean } => ({
+    visible: true,
+  }),
+  computed: {
+    dateFormatted(): string {
+      return dayjs(this.itemProp.date).format('DD MMMM YYYY')
+    }
+  },
+  methods: {
+    ...mapActions({
+      removeNote: 'note/removeNote',
+    }),
+    remove(): void {
+      this.visible = false
+      setTimeout(() => {
+        this.removeNote(this.itemProp.id)
+      }, 500)
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import './assets/style/_variables';
+
+.is-date {
+  color: dimgrey;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
